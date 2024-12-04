15e9627bcb4182b63953698cb8e96f70bf2ae937
Raajkumarrai <rrai86189@gmail.com>
2024-12-04 22:26:01 +0545

    dark  mode added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar title="React App" />
       <div className="container my-5">
         <TextForm heading="This is heading of text field" />
       </div>
+      <About />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
